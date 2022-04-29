d5f295028eefe0e93c10257411f7b9e06b8a0cef
VANTHO15 <nguyenvanthochutsinivesoss15@gmail.com>
2022-04-29 18:19:42 +0700

    tho

---
+++ BLE/07_aaa_ble_app_multirole/aaa_ble_app_multirole/app_config.h
@@ -0,0 +1,57 @@
+#ifndef __APP_CONFIG_H
+#define __APP_CONFIG_H
+
+// <e> BLE_NUS_ENABLED - ble_nus - Nordic UART Service
+//==========================================================
+#ifndef BLE_NUS_ENABLED
+#define BLE_NUS_ENABLED 1
+#endif
+
+#ifndef BLE_NUS_C_ENABLED
+#define BLE_NUS_C_ENABLED 1
+#endif
+
+#ifndef BLE_CUS_BLE_OBSERVER_PRIO
+#define BLE_CUS_BLE_OBSERVER_PRIO 2
+#endif
+
+// <o> NRF_SDH_BLE_VS_UUID_COUNT - The number of vendor-specific UUIDs. 
+#ifndef NRF_SDH_BLE_VS_UUID_COUNT
+#define NRF_SDH_BLE_VS_UUID_COUNT 2
+#endif
+
+// <o> NRF_SDH_BLE_PERIPHERAL_LINK_COUNT - Maximum number of peripheral links. 
+#ifndef NRF_SDH_BLE_PERIPHERAL_LINK_COUNT
+#define NRF_SDH_BLE_PERIPHERAL_LINK_COUNT 1
+#endif
+
+// <o> NRF_SDH_BLE_CENTRAL_LINK_COUNT - Maximum number of central links. 
+#ifndef NRF_SDH_BLE_CENTRAL_LINK_COUNT
+#define NRF_SDH_BLE_CENTRAL_LINK_COUNT 1
+#endif
+
+// <o> NRF_SDH_BLE_TOTAL_LINK_COUNT - Total link count. 
+// <i> Maximum number of total concurrent connections using the default configuration.
+
+#ifndef NRF_SDH_BLE_TOTAL_LINK_COUNT
+#define NRF_SDH_BLE_TOTAL_LINK_COUNT 2
+#endif
+
+// <o> NRF_SDH_BLE_GATT_MAX_MTU_SIZE - Static maximum MTU size. 
+#ifndef NRF_SDH_BLE_GATT_MAX_MTU_SIZE
+#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE 247
+#endif
+
+#ifndef NRF_BLE_LESC_ENABLED
+#define NRF_BLE_LESC_ENABLED 0
+#endif
+
+#ifndef PEER_MANAGER_ENABLED
+#define PEER_MANAGER_ENABLED 0
+#endif
+
+#ifndef DEVICE_NAME
+#define DEVICE_NAME "Deviot BLE Multi Role"
+#endif
+
+#endif
---
+++ BLE/07_aaa_ble_app_multirole/aaa_ble_app_multirole/pca10040/s132/arm5_no_packs/_build/main.d
@@ -0,0 +1,6 @@
+.\_build\main.o: ..\..\..\main.c
+.\_build\main.o: ..\config\sdk_config.h
+.\_build\main.o: ..\..\..\..\aaa_ble_app_multirole\app_config.h
+.\_build\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\_build\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
